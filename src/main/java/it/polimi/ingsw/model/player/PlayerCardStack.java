c012dbc9af0ad3b1cb1e76e29ce535ecb1c41f74
BrunoMor99 <bruno.morelli@me.com>
2021-03-23 23:39:11 +0100

    added PlayerCardStack

--- src/main/java/it/polimi/ingsw/model/player/PlayerCardStack.java
+++ src/main/java/it/polimi/ingsw/model/player/PlayerCardStack.java
@@ -1,5 +1,35 @@
 package it.polimi.ingsw.model.player;
 
+import it.polimi.ingsw.model.shared.CardType;
+import it.polimi.ingsw.model.shared.DevelopmentCard;
+
+import java.util.ArrayList;
+
 public class PlayerCardStack {
 
+    private ArrayList<DevelopmentCard> cards = new ArrayList<>();
+
+
+    public DevelopmentCard getTopCard(){
+        return cards.get(cards.size()-1);
+    }
+
+    public ArrayList<DevelopmentCard> getCards() {
+        return cards;
+    }
+
+    public boolean canPlaceCard(DevelopmentCard card){
+        if(cards.isEmpty())return true;
+        else {
+            if ((card.getLevel()>getTopCard().getLevel())&&(card.getCardType()==getTopCard().getCardType()))return true;
+            else return false;
+        }
+    }
+
+    public void placeCard(DevelopmentCard card){
+        if (canPlaceCard(card)){
+            cards.add(card);
+        }
+
+    }
 }
