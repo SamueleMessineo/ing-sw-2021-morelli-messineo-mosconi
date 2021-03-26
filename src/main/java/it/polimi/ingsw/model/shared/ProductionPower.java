2b6e751e564565e9a55f78824c410e1179a4603f
SamueleMessineo <10616398@polimi.it>
2021-03-26 16:52:57 +0100

    add javadoc of shared classes and define game class

--- src/main/java/it/polimi/ingsw/model/shared/ProductionPower.java
+++ src/main/java/it/polimi/ingsw/model/shared/ProductionPower.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw.model.shared;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The productionPower class represents the production power of the Development Card
+ */
+
 public class ProductionPower {
     private final List<Resource> input;
     private final List<Resource> output;
@@ -12,10 +16,18 @@ public class ProductionPower {
         this.output=output;
     }
 
+    /**
+     * Fetch the resources to pay to activate the production power
+     * @return A list of the Resources
+     */
     public List<Resource> getInput(){
         return input;
     }
 
+    /**
+     * Fetch the resources received from the production power
+     * @return A list of the Resources
+     */
     public List<Resource> getOutput(){
         return output;
     }
--- src/main/java/it/polimi/ingsw/model/shared/SoloActionType.java
+++ src/main/java/it/polimi/ingsw/model/shared/SoloActionType.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.shared;
 
+/**
+ * Defines the possible solo action token
+ */
 public enum SoloActionType {
     GREEN, YELLOW,PURPLE, BLUE, PLUS_ONE,PLUS_TWO,
 }
