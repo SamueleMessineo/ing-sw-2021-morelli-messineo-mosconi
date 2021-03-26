632a092a277b995143c24e0f78e9df86c3c38e84
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-03-26 16:32:48 +0100

    add player class

---
+++ src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -0,0 +1,71 @@
+package it.polimi.ingsw.model.player;
+
+import it.polimi.ingsw.model.shared.CardType;
+import it.polimi.ingsw.model.shared.LeaderCard;
+import it.polimi.ingsw.model.shared.Resource;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Player {
+    private final String username;
+    private int score = 0;
+    private boolean playing = false;
+    private final FaithTrack faithTrack;
+    private final PlayerBoard playerBoard;
+    private ArrayList<LeaderCard> leaderCards;
+    private ArrayList<LeaderCard> playedLeaderCards;
+
+    public Player(String username) {
+        this.username = username;
+        faithTrack = new FaithTrack();
+        playerBoard = new PlayerBoard();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void updateScore(int score) {
+        this.score += score;
+    }
+
+    public boolean isPlaying() {
+        return playing;
+    }
+
+    public FaithTrack getFaithTrack() {
+        return faithTrack;
+    }
+
+    public PlayerBoard getPlayerBoard() {
+        return playerBoard;
+    }
+
+    public ArrayList<LeaderCard> getLeaderCards() {
+        return leaderCards;
+    }
+
+    public ArrayList<LeaderCard> getPlayedLeaderCards() {
+        return playedLeaderCards;
+    }
+
+    public boolean canPlayLeader(int leaderIndex) {
+        LeaderCard leader = leaderCards.get(leaderIndex);
+
+        Map<Resource, Integer> resourceRequirements = leader.getResourceRequirements();
+        Map<Resource, Integer> allResources = playerBoard.getResources();
+        for (Resource resource : allResources.keySet()) {
+            if (allResources.get(resource) < resourceRequirements.get(resource)) return false;
+        }
+
+        Map<CardType, Integer> cardRequirements = leader.getCardRequirements();
+
+        return true;
+    }
+}
