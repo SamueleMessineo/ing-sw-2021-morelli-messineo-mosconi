8ffdb960594de78666311f33bc9c112c067e78ef
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-04-23 19:35:08 +0200

    refactor cli methods

--- src/main/java/it/polimi/ingsw/view/CLI.java
+++ src/main/java/it/polimi/ingsw/view/CLI.java
@@ -8,6 +8,7 @@ import it.polimi.ingsw.network.setup.JoinPublicRoomMessage;
 import it.polimi.ingsw.network.setup.Room;
 
 import java.io.PrintStream;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,8 +18,7 @@ public class CLI implements UI {
     private final Scanner input;
     private final PrintStream output;
     private String username;
-
-
+    
     public CLI(Client client) {
         this.client = client;
         input = new Scanner(System.in);
@@ -34,58 +34,30 @@ public class CLI implements UI {
         username = input.nextLine();
         output.println("Welcome" + username + ", nice to meet you");
         output.println("Only online game available for now");
+
         output.println("Do wou want to create a room or join an existing one?");
-        output.println("1: create\n2: join");
-        selection = input.nextInt();
-        while (selection != 1 && selection != 2) {
-            output.println("selection not valid");
-            output.println("1: create\n2: join");
-            selection = input.nextInt();
-        }
+        selection = askIntegerInput("1: create\n2: join", 1, 2);
+
         if (selection == 1) {
-            output.println("How many players is this game for?");
-            int playersNum = input.nextInt();
-
-            while (playersNum <= 0 || playersNum > 4) {
-                output.println("Invalid number, retry");
-                playersNum = input.nextInt();
-            }
-
-            input.nextLine();//non ha senso ma senza non funziona
+            int playersNum = askIntegerInput("How many players is this game for?",1,4);
 
             output.println("Is this a private game? [y/n]");
             boolean privateGame = input.nextLine().toLowerCase().startsWith("y");
             client.sendMessage(new CreateRoomMessage(privateGame, playersNum, username));
-
-
         }
         else {
             output.println("Do you have a RoomID or do you want to join a public game");
-            output.println("1: RoomId, 2: PublicGame");
-            selection = input.nextInt();
-            while (selection != 1 && selection != 2) {
-                output.println("selection not valid");
-                output.println("1: create\n2: join");
-                selection = input.nextInt();
-            }
+            selection = askIntegerInput("1: RoomId, 2: PublicGame", 1, 2);
+
             if (selection == 1) {
-                output.println("Insert roomId");
-                int roomId = input.nextInt();
+                int roomId = askIntegerInput("Insert roomId", 1000, 9999);
                 client.sendMessage(new JoinPrivateRoomMessage(roomId, username));
             }
             else {
-                output.println("Insert desired number of players [0 for random]");
-                int playersNumber = input.nextInt();
-                while (playersNumber < 0 || playersNumber > 4){
-                    output.println("Invalid number");
-                    output.println("Insert desired number of players [0 for random]");
-                    playersNumber = input.nextInt();
-                    }
+                int playersNumber = askIntegerInput("Insert desired number of players [0 for random]", 0, 4);
                 client.sendMessage(new JoinPublicRoomMessage(playersNumber, username));
             }
-
         }
-
     }
 
     @Override
@@ -95,5 +67,23 @@ public class CLI implements UI {
         output.println(playersNum);
         output.println(RoomId);
 
+    }
+
+    public int askIntegerInput(String message, int minBoundary, int maxBoundary) {
+        int selection;
+        while (true) {
+            output.println(message);
+            try {
+                selection = Integer.parseInt(input.nextLine());
+                if (selection < minBoundary || selection > maxBoundary) {
+                    throw new InvalidParameterException();
+                } else {
+                    break;
+                }
+            } catch (NumberFormatException | InvalidParameterException e) {
+                output.println("selection not valid");
+            }
+        }
+        return selection;
     }
 }
