aecad01c16e114e262c292cd72bf6ead22c6248a
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-04-30 15:25:49 +0200

    create connection scene

--- src/main/java/it/polimi/ingsw/view/GUI.java
+++ src/main/java/it/polimi/ingsw/view/GUI.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.view;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.shared.LeaderCard;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -13,9 +15,10 @@ import java.util.List;
 public class GUI extends Application implements UI {
     @Override
     public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("scenes/connect.fxml"));
         stage.setTitle("Masters of Renaissance");
-        Label label = new Label("Welcome to the game!");
-        Scene scene = new Scene(label, 1200, 700);
+        //Label label = new Label("Welcome to the game!");
+        Scene scene = new Scene(root);
         stage.setScene(scene);
 
         stage.show();
