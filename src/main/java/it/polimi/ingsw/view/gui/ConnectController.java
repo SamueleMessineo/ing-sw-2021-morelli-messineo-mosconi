63b00fb7c6b87c46e8ee24f21e3a6746c7f43cee
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-05-15 11:40:13 +0200

    accept connection input

--- src/main/java/it/polimi/ingsw/view/gui/ConnectController.java
+++ src/main/java/it/polimi/ingsw/view/gui/ConnectController.java
@@ -1,7 +1,27 @@
 package it.polimi.ingsw.view.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class ConnectController implements SceneController {
     private GUI gui;
+
+    @FXML
+    private TextField ipInput;
+
+    @FXML
+    private TextField portInput;
+
+    @FXML
+    private Text errorMessage;
+
+    @FXML
+    void connect(ActionEvent event) {
+        errorMessage.setText("trying to connect to " + ipInput.getText() + " on port " + portInput.getText());
+    }
+
     @Override
     public void setGUI(GUI gui) {
         this.gui = gui;
