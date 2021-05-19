f6ed726a091776beac83bd07d4d127d08b643bf1
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-05-19 18:21:55 +0200

    dynamically disable buttons when selecting initial resources, send selection when done

--- src/main/java/it/polimi/ingsw/view/gui/InitialResourcesController.java
+++ src/main/java/it/polimi/ingsw/view/gui/InitialResourcesController.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.view.gui;
 
 import it.polimi.ingsw.model.shared.Resource;
+import it.polimi.ingsw.network.game.SelectInitialResourceResponseMessage;
 import it.polimi.ingsw.utils.GameUtils;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -23,33 +24,65 @@ public class InitialResourcesController implements SceneController {
     private Text amountText;
     @FXML
     private HBox resourcesList;
+    @FXML
+    private Button confirmButton;
+    private int toSelect;
     private final Map<Resource, Integer> selectedResources = GameUtils.emptyResourceMap();
     private final Map<Resource, Text> selectedAmounts = new HashMap<>();
+    private int totalResourcesSelected = 0;
+    private final Map<Resource, Map<String, Button>> resourceButtonsMap = new HashMap<>();
 
     void updateCount(Resource resource, boolean increment) {
-        System.out.println("update count " + resource + " increment? " + increment);
         GameUtils.incrementValueInResourceMap(selectedResources, resource, increment ? 1 : -1);
+        totalResourcesSelected += increment ? 1 : -1;
+        if (increment){
+            resourceButtonsMap.get(resource).get("minus").setDisable(false);
+            if (totalResourcesSelected == toSelect) {
+                confirmButton.setDisable(false);
+                for (Resource r : resourceButtonsMap.keySet()) {
+                    resourceButtonsMap.get(r).get("plus").setDisable(true);
+                }
+            }
+        } else {
+            confirmButton.setDisable(true);
+            for (Resource r : resourceButtonsMap.keySet()) {
+                resourceButtonsMap.get(r).get("plus").setDisable(false);
+            }
+            if (selectedResources.get(resource) < 1) {
+                resourceButtonsMap.get(resource).get("minus").setDisable(true);
+            }
+        }
         selectedAmounts.get(resource).setText(selectedResources.get(resource).toString());
     }
 
     @FXML
     void confirm(ActionEvent event) {
-
+        if (totalResourcesSelected == toSelect) {
+            List<Resource> selected = new ArrayList<>();
+            for (Map.Entry<Resource, Integer> entry : selectedResources.entrySet()) {
+                if (entry.getValue() > 0) {
+                    for (int i = 0; i < entry.getValue(); i++) {
+                        selected.add(entry.getKey());
+                    }
+                }
+            }
+            gui.getClient().sendMessage(new SelectInitialResourceResponseMessage(selected));
+        }
     }
 
     public void displayList(List<Resource> resources, int amount) {
         Platform.runLater(() -> {
+            toSelect = amount;
             amountText.setText("Select " + amount + " resources and confirm");
             resourcesList.getChildren().clear();
             for (Resource resource : resources) {
+                resourceButtonsMap.put(resource, new HashMap<>());
                 VBox resourceContainer = new VBox();
                 resourceContainer.setAlignment(Pos.CENTER);
                 resourceContainer.setSpacing(5);
                 System.out.println("create resource vbox");
-                System.out.println(resource.name().toLowerCase());
-                Image image  = null;
                 try {
-                    image = new Image(new FileInputStream("src/main/resources/images/punchboard/" + resource.name().toLowerCase() + ".png"));
+                    Image image = new Image(new FileInputStream("src/main/resources/images/punchboard/" + resource.name().toLowerCase() + ".png"));
                     System.out.println("loaded image");
                     ImageView imageView = new ImageView(image);
                     imageView.setScaleX(0.5);
@@ -64,9 +97,11 @@ public class InitialResourcesController implements SceneController {
                 buttonContainer.setSpacing(10);
                 Button minusButton = new Button("-");
                 minusButton.setOnAction(actionEvent -> updateCount(resource, false));
-//                minusButton.setDisable(true);
+                minusButton.setDisable(true);
+                resourceButtonsMap.get(resource).put("minus", minusButton);
                 Button plusButton = new Button("+");
                 plusButton.setOnAction(actionEvent -> updateCount(resource, true));
+                resourceButtonsMap.get(resource).put("plus", plusButton);
                 Text selectedAmountText = new Text(selectedResources.get(resource).toString());
                 selectedAmounts.put(resource, selectedAmountText);
                 buttonContainer.getChildren().addAll(Arrays.asList(minusButton, selectedAmountText, plusButton));
