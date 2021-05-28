c4a86f8e717ee186c8f6666d7c2bba0c3cddf71f
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-05-28 15:34:39 +0200

    use Platform.runLater in WarehouseController

--- src/main/java/it/polimi/ingsw/view/gui/WarehouseController.java
+++ src/main/java/it/polimi/ingsw/view/gui/WarehouseController.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.model.player.Warehouse;
 import it.polimi.ingsw.network.game.SelectMoveResponseMessage;
 import it.polimi.ingsw.network.game.SwitchShelvesResponseMessage;
 import it.polimi.ingsw.utils.GameUtils;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -30,71 +31,73 @@ public class WarehouseController implements SceneController{
     @FXML
     private HBox buttonsContainer;
 
-    public void load(Warehouse warehouse){
-        // display the warehouse
-        container.getChildren().clear();
-        shelfContainerMap = new HashMap<>();
-        HBox warehouseContainer = new HBox();
-        VBox normalShelvesContainer = new VBox();
-        VBox extraShelvesContainer = new VBox();
-        for (String shelfName : warehouse.getShelfNames()) {
-            Shelf shelf = warehouse.getShelf(shelfName);
-            if (Arrays.asList("bottom", "middle", "top").contains(shelfName)) {
-                HBox shelfResourcesContainer = new HBox();
-                shelfResourcesContainer.setPrefSize(129, 46);
-                shelfResourcesContainer.setAlignment(Pos.CENTER);
-                shelfResourcesContainer.setPadding(new Insets(0, 10, 0, 10));
-                shelfResourcesContainer.setSpacing(10);
-                for (int i = 0; i < shelf.getResourceNumber(); i++) {
-                    ImageView resourceImageView = GameUtils.getImageView(shelf.getResourceType());
-                    resourceImageView.setFitWidth(40);
-                    resourceImageView.setFitHeight(40);
-                    shelfResourcesContainer.getChildren().add(resourceImageView);
+    public void load(Warehouse warehouse) {
+        Platform.runLater(() -> {
+            // display the warehouse
+            container.getChildren().clear();
+            shelfContainerMap = new HashMap<>();
+            HBox warehouseContainer = new HBox();
+            VBox normalShelvesContainer = new VBox();
+            VBox extraShelvesContainer = new VBox();
+            for (String shelfName : warehouse.getShelfNames()) {
+                Shelf shelf = warehouse.getShelf(shelfName);
+                if (Arrays.asList("bottom", "middle", "top").contains(shelfName)) {
+                    HBox shelfResourcesContainer = new HBox();
+                    shelfResourcesContainer.setPrefSize(129, 46);
+                    shelfResourcesContainer.setAlignment(Pos.CENTER);
+                    shelfResourcesContainer.setPadding(new Insets(0, 10, 0, 10));
+                    shelfResourcesContainer.setSpacing(10);
+                    for (int i = 0; i < shelf.getResourceNumber(); i++) {
+                        ImageView resourceImageView = GameUtils.getImageView(shelf.getResourceType());
+                        resourceImageView.setFitWidth(40);
+                        resourceImageView.setFitHeight(40);
+                        shelfResourcesContainer.getChildren().add(resourceImageView);
+                    }
+                    shelfContainerMap.put(shelfName, shelfResourcesContainer);
+                    normalShelvesContainer.getChildren().add(shelfResourcesContainer);
+                } else {
+                    AnchorPane extraShelfOuterContainer = new AnchorPane();
+                    Image extraShelfImage = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(
+                            "images/board/extra_" + shelf.getResourceType().name().toLowerCase() + ".png")));
+                    ImageView extraShelfImageView = new ImageView(extraShelfImage);
+                    extraShelfImageView.setPreserveRatio(true);
+                    extraShelfImageView.setFitWidth(135);
+                    extraShelfOuterContainer.setPrefSize(135, 60);
+                    extraShelfOuterContainer.getChildren().add(extraShelfImageView);
+                    HBox extraShelfResourcesContainer = new HBox();
+                    extraShelfResourcesContainer.setPrefSize(
+                            extraShelfOuterContainer.getPrefWidth(), extraShelfOuterContainer.getPrefHeight());
+                    extraShelfResourcesContainer.setLayoutX(0);
+                    extraShelfResourcesContainer.setLayoutY(0);
+                    extraShelfResourcesContainer.setPadding(new Insets(0, 0, 0, 15));
+                    extraShelfResourcesContainer.setSpacing(25);
+                    extraShelfResourcesContainer.setAlignment(Pos.CENTER_LEFT);
+                    for (int i = 0; i < shelf.getResourceNumber(); i++) {
+                        ImageView resourceImageView = GameUtils.getImageView(shelf.getResourceType());
+                        resourceImageView.setFitWidth(40);
+                        resourceImageView.setFitHeight(40);
+                        extraShelfResourcesContainer.getChildren().add(resourceImageView);
+                    }
+                    shelfContainerMap.put(shelfName, extraShelfResourcesContainer);
+                    extraShelfOuterContainer.getChildren().add(extraShelfResourcesContainer);
+                    extraShelvesContainer.getChildren().add(extraShelfOuterContainer);
                 }
-                shelfContainerMap.put(shelfName, shelfResourcesContainer);
-                normalShelvesContainer.getChildren().add(shelfResourcesContainer);
-            }else {
-                AnchorPane extraShelfOuterContainer = new AnchorPane();
-                Image extraShelfImage = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(
-                        "images/board/extra_" + shelf.getResourceType().name().toLowerCase() + ".png")));
-                ImageView extraShelfImageView = new ImageView(extraShelfImage);
-                extraShelfImageView.setPreserveRatio(true);
-                extraShelfImageView.setFitWidth(135);
-                extraShelfOuterContainer.setPrefSize(135, 60);
-                extraShelfOuterContainer.getChildren().add(extraShelfImageView);
-                HBox extraShelfResourcesContainer = new HBox();
-                extraShelfResourcesContainer.setPrefSize(
-                        extraShelfOuterContainer.getPrefWidth(), extraShelfOuterContainer.getPrefHeight());
-                extraShelfResourcesContainer.setLayoutX(0);
-                extraShelfResourcesContainer.setLayoutY(0);
-                extraShelfResourcesContainer.setPadding(new Insets(0, 0, 0, 15));
-                extraShelfResourcesContainer.setSpacing(25);
-                extraShelfResourcesContainer.setAlignment(Pos.CENTER_LEFT);
-                for (int i = 0; i < shelf.getResourceNumber(); i++) {
-                    ImageView resourceImageView = GameUtils.getImageView(shelf.getResourceType());
-                    resourceImageView.setFitWidth(40);
-                    resourceImageView.setFitHeight(40);
-                    extraShelfResourcesContainer.getChildren().add(resourceImageView);
-                }
-                shelfContainerMap.put(shelfName, extraShelfResourcesContainer);
-                extraShelfOuterContainer.getChildren().add(extraShelfResourcesContainer);
-                extraShelvesContainer.getChildren().add(extraShelfOuterContainer);
             }
-        }
-        normalShelvesContainer.setPrefSize(165, 177);
-        normalShelvesContainer.setSpacing(10);
-        normalShelvesContainer.setAlignment(Pos.BOTTOM_CENTER);
-        warehouseContainer.getChildren().add(normalShelvesContainer);
+            normalShelvesContainer.setPrefSize(165, 177);
+            normalShelvesContainer.setSpacing(10);
+            normalShelvesContainer.setAlignment(Pos.BOTTOM_CENTER);
+            warehouseContainer.getChildren().add(normalShelvesContainer);
 
-        extraShelvesContainer.setPrefSize(147, 177);
-        extraShelvesContainer.setSpacing(25);
-        extraShelvesContainer.setAlignment(Pos.CENTER);
-        warehouseContainer.getChildren().add(extraShelvesContainer);
-        warehouseContainer.setLayoutX(9);
-        warehouseContainer.setLayoutY(501);
-        warehouseContainer.setSpacing(36);
-        warehouseContainer.setAlignment(Pos.CENTER);
-        container.getChildren().addAll(warehouseContainer, buttonsContainer);
+            extraShelvesContainer.setPrefSize(147, 177);
+            extraShelvesContainer.setSpacing(25);
+            extraShelvesContainer.setAlignment(Pos.CENTER);
+            warehouseContainer.getChildren().add(extraShelvesContainer);
+            warehouseContainer.setLayoutX(9);
+            warehouseContainer.setLayoutY(501);
+            warehouseContainer.setSpacing(36);
+            warehouseContainer.setAlignment(Pos.CENTER);
+            container.getChildren().addAll(warehouseContainer, buttonsContainer);
+        });
     }
 
     @FXML
@@ -103,37 +106,41 @@ public class WarehouseController implements SceneController{
     }
 
     void allowSwitch(List<String> shelves) {
-        selected = new ArrayList<>();
-        for (String shelfName : shelves) {
-            shelfContainerMap.get(shelfName).setOnMouseClicked(event -> select(shelfName));
-            shelfContainerMap.get(shelfName).setCursor(Cursor.HAND);
-        }
-        container.getChildren().remove(buttonsContainer);
-        confirmButton = new Button("Confirm");
-        confirmButton.getStyleClass().add("flat-button");
-        confirmButton.setOnAction(event -> confirm());
-        confirmButton.setDisable(true);
-        HBox newButtonContainer = new HBox();
-        newButtonContainer.setAlignment(Pos.CENTER);
-        newButtonContainer.getChildren().add(confirmButton);
-        container.getChildren().add(newButtonContainer);
+        Platform.runLater(() -> {
+            selected = new ArrayList<>();
+            for (String shelfName : shelves) {
+                shelfContainerMap.get(shelfName).setOnMouseClicked(event -> select(shelfName));
+                shelfContainerMap.get(shelfName).setCursor(Cursor.HAND);
+            }
+            container.getChildren().remove(buttonsContainer);
+            confirmButton = new Button("Confirm");
+            confirmButton.getStyleClass().add("flat-button");
+            confirmButton.setOnAction(event -> confirm());
+            confirmButton.setDisable(true);
+            HBox newButtonContainer = new HBox();
+            newButtonContainer.setAlignment(Pos.CENTER);
+            newButtonContainer.getChildren().add(confirmButton);
+            container.getChildren().add(newButtonContainer);
+        });
     }
 
     void select(String shelfName) {
-        if (selected.contains(shelfName)) {
-            selected.remove(shelfName);
-            shelfContainerMap.get(shelfName).setStyle("");
-            confirmButton.setDisable(true);
-        } else {
-            if (selected.size() > 1) {
-                shelfContainerMap.get(selected.get(0)).setStyle("");
-                selected.remove(0);
+        Platform.runLater(() -> {
+            if (selected.contains(shelfName)) {
+                selected.remove(shelfName);
+                shelfContainerMap.get(shelfName).setStyle("");
+                confirmButton.setDisable(true);
+            } else {
+                if (selected.size() > 1) {
+                    shelfContainerMap.get(selected.get(0)).setStyle("");
+                    selected.remove(0);
+                }
+                selected.add(shelfName);
+                shelfContainerMap.get(shelfName).setStyle("-fx-border-color: red;" +
+                        "-fx-border-width: 2; -fx-border-style: solid inside;");
+                if (selected.size() == 2) confirmButton.setDisable(false);
             }
-            selected.add(shelfName);
-            shelfContainerMap.get(shelfName).setStyle("-fx-border-color: red;" +
-                    "-fx-border-width: 2; -fx-border-style: solid inside;");
-            if (selected.size() == 2) confirmButton.setDisable(false);
-        }
+        });
     }
 
     @FXML
