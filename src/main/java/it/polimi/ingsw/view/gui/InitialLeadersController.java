576ee677787472cfd599673d82c9bb9c30efed7d
BrunoMor99 <bruno.morelli@me.com>
2021-05-30 22:42:44 +0200

    add possibility of peeking market and marbles when selecting resources in gui

--- src/main/java/it/polimi/ingsw/view/gui/InitialLeadersController.java
+++ src/main/java/it/polimi/ingsw/view/gui/InitialLeadersController.java
@@ -162,4 +162,6 @@ public class InitialLeadersController implements SceneController{
             }
         });
     }
+
+
 }
--- src/main/java/it/polimi/ingsw/view/gui/InitialResourcesController.java
+++ src/main/java/it/polimi/ingsw/view/gui/InitialResourcesController.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.model.market.Marble;
+import it.polimi.ingsw.model.market.MarbleStructure;
+import it.polimi.ingsw.model.market.MarketCardStack;
+import it.polimi.ingsw.model.shared.DevelopmentCard;
 import it.polimi.ingsw.model.shared.Resource;
 import it.polimi.ingsw.network.game.SelectInitialResourceResponseMessage;
 import it.polimi.ingsw.utils.GameUtils;
@@ -7,13 +11,20 @@ import it.polimi.ingsw.utils.ResourceManager;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.util.*;
 
 public class InitialResourcesController implements SceneController {
@@ -102,6 +113,84 @@ public class InitialResourcesController implements SceneController {
                 resourceContainer.getChildren().add(buttonContainer);
                 resourcesList.getChildren().add(resourceContainer);
             }
+        });
+    }
+
+    public void peekMarket(ActionEvent actionEvent) {
+        List<MarketCardStack> cardStacks = gui.getGame().getMarket().getCardsGrid();
+        VBox popupContainer = new VBox();
+
+        popupContainer.getChildren().clear();
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getClassLoader().getResource("scenes/cards-grid.fxml"));
+        GridPane cardsGrid;
+        try {
+            cardsGrid = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        cardsGrid.setScaleX(1.9);
+        cardsGrid.setScaleY(1.9);
+        popupContainer.setAlignment(Pos.CENTER);
+        popupContainer.setSpacing(160);
+        popupContainer.setPadding(new Insets(0, 0, 30, 0));
+        popupContainer.getChildren().add(cardsGrid);
+        cardsGrid.getChildren().clear();
+        for (MarketCardStack stack : cardStacks) {
+            if (!stack.isEmpty()) {
+                DevelopmentCard topCard = stack.peek();
+                ImageView cardImageView = GameUtils.getImageView(topCard);
+                cardImageView.setFitWidth(200);
+                cardImageView.setFitHeight(95);
+                GridPane.setValignment(cardImageView, VPos.CENTER);
+                GridPane.setHalignment(cardImageView, HPos.CENTER);
+                int cardX = (cardStacks.indexOf(stack)) / 4;
+                int cardY = (cardStacks.indexOf(stack) % 4);
+                cardsGrid.add(cardImageView, cardY, cardX);
+            }
+        }
+        gui.getPopupStage().setWidth(650);
+        gui.getPopupStage().setHeight(650);
+        gui.displayPopup(popupContainer);
+    }
+
+    public void peekMarbles(ActionEvent actionEvent) {
+        Platform.runLater(() -> {
+            VBox popupContainer = new VBox();
+            Marble extra = gui.getGame().getMarket().getMarbleStructure().getExtraMarble();
+            List<Marble> marbles = gui.getGame().getMarket().getMarbleStructure().getMarbles();
+            HBox extraContainer = new HBox();
+            GridPane grid = new GridPane();
+            Image extraImage = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(
+                    "images/marbles/" + extra.name().toLowerCase() + ".png")));
+            int marbleSize = 60;
+            ImageView extraImageView = new ImageView(extraImage);
+            extraImageView.setFitHeight(marbleSize);
+            extraImageView.setFitWidth(marbleSize);
+            extraContainer.getChildren().add(extraImageView);
+            for (int i = 0; i < marbles.size(); i++) {
+                Image marbleImage = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(
+                        "images/marbles/" + marbles.get(i).name().toLowerCase() + ".png")));
+                ImageView marbleImageView = new ImageView(marbleImage);
+                marbleImageView.setPreserveRatio(true);
+                marbleImageView.setFitWidth(marbleSize);
+                marbleImageView.setFitHeight(marbleSize);
+                GridPane.setValignment(marbleImageView, VPos.CENTER);
+                GridPane.setHalignment(marbleImageView, HPos.CENTER);
+                int marbleX = (i / 4);
+                int marbleY = (i % 4);
+                grid.add(marbleImageView, marbleY, marbleX);
+            }
+
+            popupContainer.setSpacing(20);
+            popupContainer.setAlignment(Pos.CENTER);
+            popupContainer.getChildren().add(grid);
+            popupContainer.getChildren().add(extraContainer);
+
+            gui.getPopupStage().setWidth(350);
+            gui.getPopupStage().setHeight(350);
+            gui.displayPopup(popupContainer);
         });
     }
 
