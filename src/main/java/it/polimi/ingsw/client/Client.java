4b158201b8fd3d19f7dc5930c4e0c4edfc356094
BrunoMor99 <bruno.morelli@me.com>
2021-06-30 16:43:03 +0200

    add javaDoc to the client package

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -8,15 +8,27 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * The Client class, it has the calls the serverConnection to communicate with the server.
+ */
 public class Client {
     private final UI ui;
     private Socket socket;
     ServerConnection serverConnection;
 
+    /**
+     * Client class constructor.
+     */
     public Client(UI ui) {
         this.ui = ui;
     }
 
+    /**
+     * Connects to the server.
+     * @param ip server ip.
+     * @param port server port.
+     * @throws IOException if it cannot connect to the server.
+     */
     public void connect(String ip, int port) throws IOException {
         socket = new Socket(ip, port);
         serverConnection = new ServerConnection(socket, this);
@@ -24,27 +36,32 @@ public class Client {
         executor.submit(serverConnection);
     }
 
+    /**
+     * Starts the ui.
+     */
     public void run() {
-        try {
-            socket = new Socket("localhost", 31415);
-            serverConnection = new ServerConnection(socket, this);
-            ExecutorService executor = Executors.newCachedThreadPool();
-            executor.submit(serverConnection);
             ui.setup();
-        } catch (IOException e) {
-            System.out.println("Server not available :(");
-            System.out.println("Exiting...");
-        }
     }
 
+    /**
+     * Sends a new message to the server.
+     * @param m the message to send.
+     */
     public void sendMessage(Message m){
         serverConnection.sendMessage(m);
     }
 
+    /**
+     * Gets the UI.
+     * @return ui.
+     */
     public UI getUi() {
         return ui;
     }
 
+    /**
+     * Closes the server connection.
+     */
     public void closeConnection(){
         serverConnection.close();
     }
